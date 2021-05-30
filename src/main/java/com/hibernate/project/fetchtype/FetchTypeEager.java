4c6c21d84d0aea164b15a0aef7439fdd1ff6a9e1
singhhimanshu2009 <singhhimanshu2009@gmail.com>
2021-05-30 11:11:56 +0530

    FetchType EagerLoading

---
+++ src/main/java/com/hibernate/project/fetchtype/FetchTypeEager.java
@@ -0,0 +1,29 @@
+package com.hibernate.project.fetchtype;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.project.onetomany.Question;
+
+public class FetchTypeEager {
+	public static void main(String[] args) {
+		
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+		Session s = factory.openSession();
+		
+		Question q = (Question)s.get(Question.class, 1001);
+		
+		//Add FetchType.Eager in Question entity for eager loading
+		System.out.println(q.getQuestionId());
+		System.out.println(q.getQuestion());
+		
+		
+		factory.close();
+		
+	}
+
+}
